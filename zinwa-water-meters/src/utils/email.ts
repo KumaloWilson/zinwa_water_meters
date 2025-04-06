d4da8b193786a473bdc399219a4886400d27540f
KumaloWilson <kumalowilson900@gmail.com>
2025-04-06 08:13:43 +0200

    email configuration

--- zinwa-water-meters/src/utils/email.ts
+++ zinwa-water-meters/src/utils/email.ts
@@ -10,6 +10,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
   },
+  tls: {
+    rejectUnauthorized: false // Disables certificate validation
+  }
 })
 
 // Email templates
