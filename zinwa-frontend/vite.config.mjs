0f621377ac042600dcae5f3ab20d08647e99d1b3
KumaloWilson <kumalowilson900@gmail.com>
2025-04-10 12:04:59 +0200

    frontend deployment done and dusted

--- zinwa-frontend/vite.config.mjs
+++ zinwa-frontend/vite.config.mjs
@@ -1,7 +1,3 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import jsconfigPaths from 'vite-jsconfig-paths';
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   const API_URL = `${env.VITE_APP_BASE_NAME}`;
@@ -9,11 +5,10 @@ export default defineConfig(({ mode }) => {
 
   return {
     server: {
-      // this ensures that the browser opens upon server start
       open: true,
-      // this sets a default port to 3000
       port: PORT,
-      host: true
+      host: true,
+      allowedHosts: ['admin-zinwa.onrender.com']
     },
     preview: {
       open: true,
@@ -23,21 +18,7 @@ export default defineConfig(({ mode }) => {
       global: 'window'
     },
     resolve: {
-      alias: [
-        // { find: '', replacement: path.resolve(__dirname, 'src') },
-        // {
-        //   find: /^~(.+)/,
-        //   replacement: path.join(process.cwd(), 'node_modules/$1')
-        // },
-        // {
-        //   find: /^src(.+)/,
-        //   replacement: path.join(process.cwd(), 'src/$1')
-        // }
-        // {
-        //   find: 'assets',
-        //   replacement: path.join(process.cwd(), 'src/assets')
-        // },
-      ]
+      alias: []
     },
     base: API_URL,
     plugins: [react(), jsconfigPaths()]
